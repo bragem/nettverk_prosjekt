76c3272550db6730e0559cfe3fd1a61ea80a5fd5
Anders <andetel@stud.ntnu.no>
2022-03-16 13:44:58 +0100

    Added javadoc to decrypt method

--- src/main/java/utils/CryptoUtil.java
+++ src/main/java/utils/CryptoUtil.java
@@ -4,6 +4,16 @@ import javax.crypto.*;
 
 public class CryptoUtil {
 
+
+    /**
+     * Returns a byte array containing the decrypted data
+     *
+     * @param encryptedData byte array of the encrypted data
+     * @param length the length of the byte array containing the encrypted data
+     * @param key the {@link SecretKey} that will be used to decrypt
+     * @return byte array containing the decrypted data
+     * @throws Exception if the key is invalid
+     */
     public static byte[] decrypt(byte[] encryptedData, int length, SecretKey key) throws Exception {
         Cipher cipher = Cipher.getInstance("AES");
         cipher.init(Cipher.DECRYPT_MODE, key);
