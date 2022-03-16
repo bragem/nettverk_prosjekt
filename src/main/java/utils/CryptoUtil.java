3fbda402c466276f917bc94768a71f281b1c0fd8
Anders <andetel@stud.ntnu.no>
2022-03-16 14:09:38 +0100

    Added a method encrypt, to encrypt data

--- src/main/java/utils/CryptoUtil.java
+++ src/main/java/utils/CryptoUtil.java
@@ -9,7 +9,7 @@ public class CryptoUtil {
      * Returns a byte array containing the decrypted data
      *
      * @param encryptedData byte array of the encrypted data
-     * @param length the length of the byte array containing the encrypted data
+     * @param length how many bytes in the array to decrypt from position 0
      * @param key the {@link SecretKey} that will be used to decrypt
      * @return byte array containing the decrypted data
      * @throws Exception if the key is invalid
@@ -19,6 +19,23 @@ public class CryptoUtil {
         cipher.init(Cipher.DECRYPT_MODE, key);
 
         return cipher.doFinal(encryptedData, 0, length);
+    }
+
+
+    /**
+     * Returns a byte array containing the encrypted data
+     *
+     * @param data byte array of the data to encrypt
+     * @param length how many bytes in the array to encrypt from position 0
+     * @param key the {@link SecretKey} to use to encrypt
+     * @return byte array
+     * @throws Exception if the key is invalid
+     */
+    public static byte[] encrypt(byte[] data, int length, SecretKey key) throws Exception {
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.ENCRYPT_MODE, key);
+
+        return cipher.doFinal(data, 0, length);
     }
 
 }
