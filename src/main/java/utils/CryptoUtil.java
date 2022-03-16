70296f3b9dd6e68092f904d3574f339fcca27721
Anders <andetel@stud.ntnu.no>
2022-03-16 13:35:39 +0100

    Added a method decrypt, that is going to decrypt a message using a secret key

--- src/main/java/utils/CryptoUtil.java
+++ src/main/java/utils/CryptoUtil.java
@@ -1,5 +1,14 @@
 package utils;
 
+import javax.crypto.*;
+
 public class CryptoUtil {
+
+    public static byte[] decrypt(byte[] encryptedData, int length, SecretKey key) throws Exception {
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.DECRYPT_MODE, key);
+
+        return cipher.doFinal(encryptedData, 0, length);
+    }
 
 }
