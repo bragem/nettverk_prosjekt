c9c2d48d26529b072cc0e7c2151c22e547196dd9
Anders Tellefsen <anders1.tellefsen@gmail.com>
2022-03-17 21:08:56 +0100

    Created methods for creating, saving and loading RSA keys

--- src/main/java/OnionNode.java
+++ src/main/java/OnionNode.java
@@ -1,8 +1,18 @@
 import javax.crypto.SecretKey;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
-import java.security.PrivateKey;
-import java.security.PublicKey;
+import java.nio.file.Files;
+import java.nio.file.InvalidPathException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.security.*;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
 import java.util.Arrays;
 import java.util.List;
 
@@ -14,8 +24,8 @@ public class OnionNode {
     private int port;
 
     // Public and private RSA keys for initial setup of connection
-    private PrivateKey privateKey;
-    private PublicKey publicKey;
+    private Key publicKey;
+    private Key privateKey;
 
     // Secret symmetric key
     private SecretKey secretKey;
@@ -41,15 +51,20 @@ public class OnionNode {
         return port;
     }
 
-    public PublicKey getPublicKey() {
-        return publicKey;
+    public String getPublicKey() {
+        return publicKey.getFormat();
     }
 
-    private void setPublicKey(PublicKey key) {
+    //TODO: Remove method below
+    public String getPrivateKey() {
+        return privateKey.getFormat();
+    }
+
+    private void setPublicKey(Key key) {
         this.publicKey = key;
     }
 
-    private void setPrivateKey(PrivateKey key) {
+    private void setPrivateKey(Key key) {
         this.privateKey = key;
     }
 
@@ -57,14 +72,91 @@ public class OnionNode {
         this.secretKey = key;
     }
 
-    public static void main(String[] args) throws UnknownHostException {
+    private void createRSA() throws NoSuchAlgorithmException, IOException {
+        KeyPairGenerator kpg = KeyPairGenerator.getInstance("RSA");
+        kpg.initialize(2048);
+
+        KeyPair kp = kpg.generateKeyPair();
+
+        PublicKey pub = kp.getPublic();
+        PrivateKey pvt = kp.getPrivate();
+        saveRSA(pub, pvt);
+    }
+
+    private void saveRSA(PublicKey pub, PrivateKey pvt) throws IOException {
+        String pubOutFile = "rsa_pub.pub";
+        String pvtOutFile = "rsa_pvt.key";
+
+        File dir = new File("./keys/");
+        boolean dirCreated = dir.mkdir();
+
+        if(dirCreated) {
+            System.out.println("Directory created");
+
+            File rsaPub = new File("./keys/" + pubOutFile);
+            File rsaPvt = new File("./keys/" + pvtOutFile);
+
+            try(FileOutputStream fosPub = new FileOutputStream(rsaPub)) {
+                fosPub.write(pub.getEncoded());
+            } catch (FileNotFoundException e) {
+                System.out.println(e);
+            }
+
+            try(FileOutputStream fosPvt = new FileOutputStream(rsaPvt)) {
+                fosPvt.write(pvt.getEncoded());
+            } catch (FileNotFoundException e) {
+                System.out.println(e);
+            }
+        }
+    }
+
+    private PrivateKey loadRSAPrivateKey() {
+
+        try {
+            Path path = Paths.get("./keys/rsa_pvt.key");
+            byte[] bytes = Files.readAllBytes(path);
+
+            PKCS8EncodedKeySpec ks = new PKCS8EncodedKeySpec(bytes);
+            KeyFactory kf = KeyFactory.getInstance("RSA");
+            PrivateKey pvt = kf.generatePrivate(ks);
+
+            return pvt;
+        } catch (IOException | NoSuchAlgorithmException | InvalidKeySpecException e) {
+            System.out.println(e);
+        }
+
+        return null;
+    }
+
+    private PublicKey loadRSAPublicKey() {
+
+        try {
+            Path path = Paths.get("./keys/rsa_pub.pub");
+            byte[] bytes = Files.readAllBytes(path);
+
+            X509EncodedKeySpec ks = new X509EncodedKeySpec(bytes);
+            KeyFactory kf = KeyFactory.getInstance("RSA");
+            PublicKey pub = kf.generatePublic(ks);
+
+            return pub;
+        } catch (IOException | NoSuchAlgorithmException | InvalidKeySpecException e) {
+            System.out.println(e);
+        }
+
+        return null;
+    }
+
+    public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
         List<String> argsList = Arrays.asList(args);
 
         if(argsList.contains("-p")) {
             int port = Integer.parseInt(argsList.get(argsList.indexOf("-p") + 1));
             OnionNode node = new OnionNode(port);
-        }
+            node.createRSA();
 
+            /*System.out.println("Public key format: \n" + node.getPublicKey() + "\n\n");
+            System.out.println("Private key format: \n" + node.getPrivateKey() + "\n\n");*/
+        }
 
     }
 }
