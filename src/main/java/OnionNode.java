65c6c45b8c1bb9e8aaa5b9eb60dfc986608085e8
Anders Tellefsen <anders1.tellefsen@gmail.com>
2022-03-24 12:57:26 +0100

    Added logger, added javadoc and change all printlines to logger

--- src/main/java/OnionNode.java
+++ src/main/java/OnionNode.java
@@ -1,3 +1,5 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import utils.CryptoUtil;
 
 import javax.crypto.*;
@@ -22,6 +24,8 @@ import java.util.regex.Pattern;
 
 public class OnionNode {
 
+    private static Logger logger = LoggerFactory.getLogger(OnionNode.class);
+
     // Network info of node
     private String IPAddress;
     private int port;
@@ -41,12 +45,18 @@ public class OnionNode {
     private String nextIP;
     private int nextPort;
 
+    /**
+     * Creates a new {@link OnionNode} object
+     *
+     * @param port the port to start the node at
+     * @throws UnknownHostException
+     */
     public OnionNode(int port) throws UnknownHostException {
         this.IPAddress = InetAddress.getByName(InetAddress.getLocalHost().getHostName()).getHostAddress();
         this.port = port;
 
-        System.out.println("Node starting...");
-        System.out.println("Node started at " + this.getIPAddress() + " on port " + this.getPort());
+        logger.info("Node starting...");
+        logger.info(String.format("Node started at {}:{}", getIPAddress(), getPort()));
     }
 
     public String getIPAddress() {
@@ -80,8 +90,7 @@ public class OnionNode {
         prevIP = connection.getRemoteSocketAddress().toString().split("[/:]")[1];
         prevPort = connection.getPort();
 
-        System.out.println("IP of connected device: " + prevIP + "\n");
-
+        logger.info(String.format("IP of connected device: {}", prevIP));
 
         int byteLength;
         byte[] bytes;
@@ -98,8 +107,8 @@ public class OnionNode {
 
 
             if("GivePK!!!".equals(tmp)) {
-                System.out.println("Received from client: " + tmp + "");
-                System.out.println("Sending public key back to client...");
+                logger.info(String.format("Received from client: {}", tmp));
+                logger.info("Sending public key back to client...");
 
                 PublicKey pk = loadRSAPublicKey();
                 byte[] stBytes = pk.getEncoded();
@@ -114,7 +123,7 @@ public class OnionNode {
                 SecretKey sk = new SecretKeySpec(decrypted, "AES");
                 setSecretKey(sk);
 
-                System.out.println("Secret key received from client");
+                logger.info("Secret key received from client");
 
                 String response = "Secret key set at node " + getIPAddress() + ":" + getPort();
                 byte[] responseBytes = response.getBytes();
@@ -131,7 +140,7 @@ public class OnionNode {
                 nextPort = Integer.parseInt(st.split("[:/]")[1]);
                 String message = st.split("[:/]")[2];
 
-                System.out.println("Received IP and Port of next device: " + nextIP + ":" + nextPort);
+                logger.info(String.format("Received IP and Port of next device: {}:{}", nextIP, nextPort));
 
                 forwardData(connection, message);
                 setupComplete = true;
@@ -140,7 +149,13 @@ public class OnionNode {
 
     }
 
-
+    /**
+     * Takes an already established connection and creates a second one, to receive from one and send on the other
+     *
+     * @param connection the {@link Socket} that is already created
+     * @param firstMessage the first message to forward to the next node
+     * @throws Exception
+     */
     public void forwardData(Socket connection, String firstMessage) throws Exception {
 
         boolean quit = false;
@@ -153,11 +168,9 @@ public class OnionNode {
 
 
         writeToNext.writeInt(firstMessage.getBytes().length);
-
         writeToNext.write(firstMessage.getBytes());
-        System.out.println("First message to next node is now sent!");
-
         writeToNext.flush();
+        logger.info("First message to next node now sent");
 
         String lastAction = "writeToNext";
 
@@ -178,17 +191,17 @@ public class OnionNode {
                     bytes = new byte[byteLength];
                     readFromNext.readFully(bytes);
 
-                    System.out.println("Received message from next node \n");
+                    logger.info("Received message from next node");
 
                     break;
                 case "readFromNext":
                     lastAction = "writeToPrev";
 
                     encrypted = CryptoUtil.encryptAES(bytes, bytes.length, getSecretKey());
-                    System.out.println("Message to previous node encrypted!");
+                    logger.info("Message to previous node encrypted!");
                     writeToPrev.writeInt(encrypted.length);
                     writeToPrev.write(encrypted);
-                    System.out.println("Message to previous node sent!");
+                    logger.info("Message to previous node sent");
 
                     break;
                 case "writeToPrev":
@@ -198,27 +211,24 @@ public class OnionNode {
                     bytes = new byte[byteLength];
                     readFromPrev.readFully(bytes);
 
-                    System.out.println("Received message from previous node \n");
+                    logger.info("Received message from previous node");
 
                     break;
                 case "readFromPrev":
                     lastAction = "writeToNext";
 
-//                    bytes = new byte[readFromPrev.readInt()];
-//                    readFromPrev.readFully(bytes);
                     decrypted = CryptoUtil.decryptAES(bytes, bytes.length, getSecretKey());
-                    System.out.println("Message to next node decrypted!");
+                    logger.info("Message to next node decrypted");
                     writeToNext.writeInt(decrypted.length);
                     writeToNext.write(decrypted);
-                    System.out.println("Message to next node sent!");
+                    logger.info("Message to next node sent");
+                    System.out.println();
 
                     break;
                 default:
-                    System.out.println("wtf?");
+                    logger.info("Something weird happened");
             }
 
-
-//            quit = true;
         }
 
     }
@@ -230,15 +240,20 @@ public class OnionNode {
      * @throws NoSuchAlgorithmException if the generator doesn't recognize the encryption algorithm
      * @throws IOException if the saveRSA method fails to save the keys
      */
-    private void createRSA() throws NoSuchAlgorithmException, IOException {
-        KeyPairGenerator kpg = KeyPairGenerator.getInstance("RSA");
-        kpg.initialize(2048);
+    private void createRSA() throws IOException {
+        try {
+            KeyPairGenerator kpg = KeyPairGenerator.getInstance("RSA");
+            kpg.initialize(2048);
 
-        KeyPair kp = kpg.generateKeyPair();
+            KeyPair kp = kpg.generateKeyPair();
 
-        PublicKey pub = kp.getPublic();
-        PrivateKey pvt = kp.getPrivate();
-        saveRSA(pub, pvt);
+            PublicKey pub = kp.getPublic();
+            PrivateKey pvt = kp.getPrivate();
+            saveRSA(pub, pvt);
+        } catch (NoSuchAlgorithmException e) {
+            logger.error(e.getMessage());
+        }
+
         //cleanUp(new File("./keys"));
     }
 
@@ -249,7 +264,7 @@ public class OnionNode {
      * @param pvt the private key
      * @throws IOException if it fails to save to file
      */
-    private void saveRSA(PublicKey pub, PrivateKey pvt) throws IOException {
+    private void saveRSA(PublicKey pub, PrivateKey pvt) {
         String pubOutFile = "rsa_pub.pub";
         String pvtOutFile = "rsa_pvt.key";
 
@@ -257,21 +272,21 @@ public class OnionNode {
         boolean dirCreated = dir.mkdir();
 
         if(dirCreated) {
-            System.out.println("Directory created");
+            logger.info("Directory created");
 
             File rsaPub = new File("./src/keys/" + pubOutFile);
             File rsaPvt = new File("./src/keys/" + pvtOutFile);
 
             try(FileOutputStream fosPub = new FileOutputStream(rsaPub)) {
                 fosPub.write(pub.getEncoded());
-            } catch (FileNotFoundException e) {
-                System.out.println(e);
+            } catch (IOException e) {
+                logger.error(e.getMessage());
             }
 
             try(FileOutputStream fosPvt = new FileOutputStream(rsaPvt)) {
                 fosPvt.write(pvt.getEncoded());
-            } catch (FileNotFoundException e) {
-                System.out.println(e);
+            } catch (IOException e) {
+                logger.error(e.getMessage());
             }
         }
     }
@@ -293,7 +308,7 @@ public class OnionNode {
 
             return pvt;
         } catch (IOException | NoSuchAlgorithmException | InvalidKeySpecException e) {
-            System.out.println(e);
+            logger.error(e.getMessage());
         }
 
         return null;
@@ -316,7 +331,7 @@ public class OnionNode {
 
             return pub;
         } catch (IOException | NoSuchAlgorithmException | InvalidKeySpecException e) {
-            System.out.println(e);
+            logger.error(e.getMessage());
         }
 
         return null;
@@ -326,8 +341,9 @@ public class OnionNode {
      * Deletes the generated public- and private key files with its folder
      *
      * @param file the {@link File} with the path to the directory of the public and private keys
+     * @return true if files and folders are deleted successfully, otherwise false
      */
-    private void cleanUp(File file) {
+    private boolean cleanUp(File file) {
         File[] contents = file.listFiles();
         if (contents != null) {
             for (File f : contents) {
@@ -336,7 +352,7 @@ public class OnionNode {
                 }
             }
         }
-        file.delete();
+        return file.delete();
     }
 
     public static void main(String[] args) throws Exception {
